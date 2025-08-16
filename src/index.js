4146fb5134da547e3a31e0b84b2b00691fb9ecc6
NataliiaSwan <shapnat69@gmail.com>
2025-08-16 16:25:02 +0300

    added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  let poemElement = document.querySelector("#poem");
+
+  new Typewriter(poemElement, {
+    strings: ["Je m'en allais, les poings dans mes poches crevées"],
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
+let formPoemElement = document.querySelector("#poem-generator-form");
+formPoemElement.addEventListener("submit", generatePoem);
